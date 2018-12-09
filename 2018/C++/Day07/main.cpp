27ebcefba701b48489ec750d3fb34a91469169f1
Jonas Karlsson <jonas@y3k.se>
2018-12-09 03:49:00 +0100

    Reversing prerequisites made solution part 1 day 7 much cleaner

--- 2018/C++/Day07/main.cpp
+++ 2018/C++/Day07/main.cpp
@@ -3,103 +3,53 @@
 #include <fstream>
 #include <set>
 #include <unordered_set>
+#include <map>
 #include <unordered_map>
 
 using namespace std;
 
-struct Instructions
-{
-    unordered_set<char> done;
-    unordered_map<char,int> following_prereq;
-    set<char> next_candidates;
-    unordered_map<char,set<char>> prerequisites;
-};
+typedef map<char,set<char>> Prerequisites;
 
-Instructions get_instructions(ifstream &infile)
+Prerequisites get_prereqs(ifstream &infile)
 {
     char prereq, follow;
-    Instructions instructions;
+    Prerequisites prerequisites;
     string u;
 
     while (infile >> u >> prereq >> u >> u >> u >> u >> u >> follow >> u >> u)
     {
-        auto found_follower = instructions.following_prereq.find(follow);
-        if (found_follower == instructions.following_prereq.end())
-        {
-            instructions.following_prereq.insert(make_pair(follow,1));
-        }
-        else
-        {
-            found_follower->second++;
-        }
-
-        auto follower_in_next = instructions.next_candidates.find(follow);
-        if (follower_in_next != instructions.next_candidates.end())
-        {
-            instructions.next_candidates.erase(follower_in_next);
-        }
-
-        auto prereq_follower = instructions.following_prereq.find(prereq);
-        if (prereq_follower == instructions.following_prereq.end())
-        {
-            instructions.next_candidates.insert(prereq);
-        }
-
-        auto found = instructions.prerequisites.find(prereq);
-        if (found == instructions.prerequisites.end())
-        {
-            set<char> fs;
-            fs.insert(follow);
-            instructions.prerequisites.insert(make_pair(prereq,fs));
-        }
-        else
-        {
-            found->second.insert(follow);
-        }
+        if (!prerequisites.count(prereq)) prerequisites[prereq] = set<char>();
+        prerequisites[follow].insert(prereq);
     }
-    return instructions;
+    return prerequisites;
 }
 
-string first(Instructions instructions)
+char get_first_available_instruction(Prerequisites prereqs)
+{
+    for (const auto & p : prereqs) {
+        if (p.second.empty()) {
+            return p.first;
+        }
+    }
+    throw runtime_error("Unreachable instructions");
+}
+
+void remove_done_instruction(Prerequisites &prereqs, char instruction)
+{
+    for (auto &p : prereqs) {
+        p.second.erase(instruction);
+    }
+}
+
+string first(Prerequisites prereqs)
 {
     string solution;
-    while (instructions.next_candidates.size() > 0)
+    while (!prereqs.empty())
     {
-        char current_instruction = *(instructions.next_candidates.begin());
-
-        solution += current_instruction;
-        instructions.done.insert(current_instruction);
-        instructions.next_candidates.erase(current_instruction);
-
-        auto found_instruction = instructions.prerequisites.find(current_instruction);
-        if (found_instruction != instructions.prerequisites.end())
-        {
-            for (auto const &candidate_instruction: found_instruction->second)
-            {
-                auto const &done = instructions.done.find(candidate_instruction);
-                if (done == instructions.done.end())
-                {
-                    auto const &follower = instructions.following_prereq.find(candidate_instruction);
-                    if (follower != instructions.following_prereq.end())
-                    {
-                        follower->second--;
-                        if (follower->second == 0)
-                        {
-                            instructions.following_prereq.erase(follower->first);
-                            instructions.next_candidates.insert(candidate_instruction);
-                        }
-                    }
-                    else
-                    {
-                        instructions.next_candidates.insert(candidate_instruction);
-                    }
-                }
-            }
-        }
-        else
-        {
-            // cout << "No more prerequisites" << endl;
-        }
+        char instruction = get_first_available_instruction(prereqs);
+        solution += instruction;
+        prereqs.erase(instruction);
+        remove_done_instruction(prereqs, instruction);
     }
     return solution;
 }
@@ -109,9 +59,8 @@ int main()
     string str;
     ifstream infile("input.txt");
 
-    Instructions instructions = get_instructions(infile);
-
-    string first_solution = first(instructions);
+    Prerequisites prereqs = get_prereqs(infile);
+    string first_solution = first(prereqs);
     cout << "first: " << first_solution << endl;
     return 0;
 }
