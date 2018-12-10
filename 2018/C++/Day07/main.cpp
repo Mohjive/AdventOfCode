0d73abf268efae5d38911160434e929fbd93d8f2
Jonas Karlsson <jonas@y3k.se>
2018-12-10 22:00:19 +0100

    Solution part 2 day 7

--- 2018/C++/Day07/main.cpp
+++ 2018/C++/Day07/main.cpp
@@ -10,6 +10,9 @@ using namespace std;
 
 typedef map<char,set<char>> Prerequisites;
 
+constexpr int num_workers = 5;
+constexpr int additional_time = 60;
+
 Prerequisites get_prereqs(ifstream &infile)
 {
     char prereq, follow;
@@ -41,17 +44,73 @@ void remove_done_instruction(Prerequisites &prereqs, char instruction)
     }
 }
 
+constexpr int get_completion_time(char instruction = 'Z')
+{
+    return additional_time + instruction - 'A' + 1;
+}
+
 string first(Prerequisites prereqs)
 {
-    string solution;
+    string instruction_sequence;
     while (!prereqs.empty())
     {
         char instruction = get_first_available_instruction(prereqs);
-        solution += instruction;
+        instruction_sequence += instruction;
         prereqs.erase(instruction);
         remove_done_instruction(prereqs, instruction);
     }
-    return solution;
+    return instruction_sequence;
+}
+
+int second(Prerequisites prereqs)
+{
+
+    int accumulated_time = 0;
+    unordered_map<char,int> workers;
+
+    int next_completion_time = get_completion_time('Z');
+    char next_complete_instruction = '\0';
+    while (!prereqs.empty())
+    {
+        while (workers.size() < num_workers)
+        {
+            char instruction;
+            try
+            {
+                instruction = get_first_available_instruction(prereqs);
+            }
+            catch (runtime_error e)
+            {
+                break;
+            }
+            prereqs.erase(instruction);
+
+            int completion_time = get_completion_time(instruction);
+            workers[instruction] = completion_time;
+            if (completion_time < next_completion_time)
+            {
+                next_completion_time = completion_time;
+                next_complete_instruction = instruction;
+            }
+        }
+
+        remove_done_instruction(prereqs, next_complete_instruction);
+        int completion_time = next_completion_time;
+        workers.erase(next_complete_instruction);
+        accumulated_time += completion_time;
+
+        next_completion_time = get_completion_time('Z');
+        for (auto &w: workers)
+        {
+            w.second -= completion_time;
+            if (w.second < next_completion_time)
+            {
+                next_completion_time = w.second;
+                next_complete_instruction = w.first;
+            }
+        }
+    }
+    return accumulated_time;
 }
 
 int main()
@@ -61,6 +120,8 @@ int main()
 
     Prerequisites prereqs = get_prereqs(infile);
     string first_solution = first(prereqs);
+    int second_solution = second(prereqs);
     cout << "first: " << first_solution << endl;
+    cout << "second: " << second_solution << endl;
     return 0;
 }
